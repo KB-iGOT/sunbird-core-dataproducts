2db4bb2a22f51c28164ccb03523623d019dfa871
Shishir Suman <74050813+shishirsuman092@users.noreply.github.com>
2025-01-07 16:23:48 +0530

    Revert "Cbrelease 4.8.20"

--- batch-models/src/main/scala/org/ekstep/analytics/dashboard/report/commsconsole/CommsReportModel.scala
+++ batch-models/src/main/scala/org/ekstep/analytics/dashboard/report/commsconsole/CommsReportModel.scala
@@ -27,18 +27,21 @@ object CommsReportModel extends AbsDashboardModel {
     val lastUpdatedOn = date_format(current_timestamp(), "dd/MM/yyyy HH:mm:ss a")
     val commsConsoleReportPath = s"${conf.commsConsoleReportPath}/${today}"
 
-    val orgDF = warehouseCache.load(conf.dwOrgTable)
+    val orgDF = spark.read.option("header", "true")
+      .csv(s"${conf.localReportDir}/${conf.orgHierarchyReportPath}/${today}-warehouse")
       .withColumn("department", when(col("ministry").isNotNull && col("department").isNull, col("mdo_name")).otherwise(col("department")))
       .withColumn("ministry", when(col("ministry").isNull && col("department").isNull, col("mdo_name")).otherwise(col("ministry")))
       .select("mdo_id", "ministry", "department", "organization")
 
-    val userDF =warehouseCache.load(conf.dwUserTable)
+    val userDF = spark.read.option("header", "true")
+      .csv(s"${conf.localReportDir}/${conf.userReportPath}/${today}-warehouse")
       //.withColumn("registrationDate", to_date(col("user_registration_date"), dateFormat1))
       .withColumn("registrationDate",  date_format(col("user_registration_date"), "dd/MM/yyyy HH:mm:ss a"))
       .select("user_id", "mdo_id", "status", "full_name", "email", "phone_number", "roles", "registrationDate", "tag", "user_registration_date")
       .join(orgDF, Seq("mdo_id"), "left")
 
-    val rawEnrollmentsDF = warehouseCache.load(conf.dwEnrollmentsTable)
+    val rawEnrollmentsDF = spark.read.option("header", "true")
+      .csv(s"${conf.localReportDir}/${conf.userEnrolmentReportPath}/${today}-warehouse")
       //.withColumn("completionDate", to_date(col("completed_on"), dateFormat2))
       .withColumn("completionDate",  date_format(col("content_last_accessed_on"), "dd/MM/yyyy HH:mm:ss a"))
     val enrollmentsDF = rawEnrollmentsDF
